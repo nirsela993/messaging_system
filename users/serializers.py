537b03a602cd74b667fa4aa73065797e09f42d16
nirsela993@gmail.com <nirsela993@gmail.com>
2019-06-24 19:35:46 +0300

    modified user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,9 @@
 from rest_framework import serializers
-from rest_framework.fields import EmailField
 
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    email = EmailField()
 
     class Meta:
         model = User
-        fields = ['email']
